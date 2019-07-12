0ba2582fd3faa02c68eeac4e76dc1f9d4ac89483
Pavel Kulik <pavel.kulik@alleninstitute.org>
2019-07-12 13:55:19 -0700

    Update travis_build.sh

--- travis_build.sh
+++ travis_build.sh
@@ -1,10 +1,9 @@
 cd ..
 git clone -b cmakeBuild https://github.com/aacuevas/plugin-GUI.git
 cd plugin-GUI/Build
-cmake ..
+cmake "Visual Studio 15 2017" Win64 ..
 MSBuild.exe open-ephys.vcxproj 
 cd ../..
-cd ni-daq/
-sh ./create_build_files.sh
-cd Build
-MSBuild.exe NI-DAQmx.vcxproj
+cd ni-daq/Build
+cmake "Visual Studio 15 2017" Win64 ..
+MSBuild.exe INSTALL.vcxproj 
