61ad0ccf37bf8e7e49a4ebc0b96daaae960fdbb4
Pavel Kulik <pavel.kulik@alleninstitute.org>
2019-08-05 13:59:24 -0700

    Fix travis_build.sh path

--- .travis/travis_build.sh
+++ .travis/travis_build.sh
@@ -1,9 +1,8 @@
-cd ..
+cd /c/Users/travis/build/
 git clone -b development https://github.com/open-ephys/plugin-GUI.git
 cd plugin-GUI/Build
 cmake -G "Visual Studio 15 2017 Win64" ..
 MSBuild.exe open-ephys.vcxproj //p:Configuration=Release //p:Platform=x64
-cd ../..
-cd nidaq-plugin/Build
+cd /c/Users/travis/build/open-ephys-plugins/nidaq-plugin/Build
 cmake -G "Visual Studio 15 2017 Win64" ..
 MSBuild.exe INSTALL.vcxproj //p:Configuration=Release //p:Platform=x64
