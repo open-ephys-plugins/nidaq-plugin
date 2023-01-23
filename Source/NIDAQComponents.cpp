7cefd0159e77699a8ce1f9411e04561df506fd2d
Josh Siegle <joshs@alleninstitute.org>
2023-01-23 11:53:14 -0800

    Remove debug output

--- Source/NIDAQComponents.cpp
+++ Source/NIDAQComponents.cpp
@@ -548,7 +548,7 @@ void NIDAQmx::run()
 			&ai_read,
 			NULL));
 
-		LOGD("arraySizeInSamps: ", arraySizeInSamps, "Samples read: ", ai_read);
+		//LOGD("arraySizeInSamps: ", arraySizeInSamps, "Samples read: ", ai_read);
 
 		if (getActiveDigitalLines() > 0)
 		{
