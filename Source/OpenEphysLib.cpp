c0cc3fe5118b385da16ec59e28448d2fecc795ac
Anjal Doshi <anjaldoshi09@gmail.com>
2023-03-13 14:19:10 -0700

    Bump plugin version to 0.3.0

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -38,7 +38,7 @@ extern "C" EXPORT void getLibInfo(Plugin::LibraryInfo* info)
 {
 	info->apiVersion = PLUGIN_API_VER;
 	info->name = "NI-DAQmx";
-	info->libVersion = "0.2.0";
+	info->libVersion = "0.3.0";
 	info->numPlugins = NUM_PLUGINS;
 }
 
